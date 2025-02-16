dca220506840891010ce45321d19fbb4c3c8c6d5
Pathum Vimukthi <104794593+Pathum-Vimukthi@users.noreply.github.com>
2025-02-16 14:19:04 +0530

    implement clock functionality with JavaScript and update HTML and CSS for integration

---
+++ clock.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second');
+const minuteHand = document.querySelector('.minute');
+const hourHand = document.querySelector('.hour');
+
+function setTime(){
+    //Get Time
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    //Set Rotation
+    const secondsRotation = seconds * 6;
+    const minutesRotation = minutes * 6;
+    const hoursRotation = (hours % 12) * 30 + 0.5 * minutes;
+
+    //Apply Rotation
+    secondHand.style.transform = `rotate(${secondsRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minutesRotation}deg)`;
+    hourHand.style.transform = `rotate(${hoursRotation}deg)`;
+}
+
+setInterval(setTime, 1000);
